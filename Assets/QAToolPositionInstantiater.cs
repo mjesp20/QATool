41e4692249c5b7254e15288f68d99fe128615569
mjesp20 <mjesp20@student.aau.dk>
2026-02-16 14:48:31 +0100

    add instantiator

--- Assets/QAToolPositionInstantiater.cs
+++ Assets/QAToolPositionInstantiater.cs
@@ -1,21 +1,53 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class QAToolPositionInstantiater : MonoBehaviour
 {
     private List<Vector3> positions;
-    
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        positions = QAToolTelemetryLoader.LoadPositions("C:\\Users\\rasmu\\Documents\\QATool\\1.jsonl");
 
+    private string filePath;
+    
+
+    int i = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        string folderPath = Path.Combine(documentsPath, "QATool");
+        filePath = Path.Combine(folderPath, $"{1}.jsonl");
+
+        positions = QAToolTelemetryLoader.LoadPositions(filePath);
+
+        GameObject oldCube = null;
+        GameObject newCube = null;
         foreach (Vector3 position in positions)
         {
-            
-            GameObject Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            Cube.transform.position = position;
+
+            newCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            newCube.transform.position = position;
+            newCube.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            newCube.transform.name = i.ToString();
+            newCube.GetComponent<Renderer>().material.color = new Color(0, 1, 0, 1);
+            newCube.GetComponent<Collider>().enabled = false;
+            i++;
+
+            if (oldCube == null)
+            {
+                oldCube = newCube;
+                continue;
+            }
+            else
+            {
+                GameObject line = new GameObject();
+                LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
+                lineRenderer.SetPositions(new[] { oldCube.transform.position, newCube.transform.position });
+                
+            }
+
+            oldCube = newCube;
         }
     }
 
